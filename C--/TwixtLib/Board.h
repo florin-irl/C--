1bda081daa69599f69c82c5610c65a0b85261931
m1toi <alex.mitoi@student.unitbv.ro>
2023-10-25 22:33:58 +0300

    create board class structure

--- C--/TwixtLib/Board.h
+++ C--/TwixtLib/Board.h
@@ -1,9 +1,20 @@
 #pragma once
+#include "Peg.h"
+#include <vector>
 class Board
 {
 public:
 	
 private:
+	int m_boardSize;
+	Peg m_turn;
 
+	int m_nrOfPegsPlayer1;
+	int m_nrOfBridgesPlayer1;
+	int m_nrOfPegsPlayer2;
+	int m_nrOfBridgesPlayer2;
+
+
+	std::vector<std::vector<Peg*>> m_board;
 };
 
