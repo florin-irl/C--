cfb2d74677437c87a4cb3ad7698e6f99e0d1cf07
m1toi <alex.mitoi@student.unitbv.ro>
2023-11-03 16:04:39 +0200

    update Board default constructor

--- C--/TwixtLib/Board.cpp
+++ C--/TwixtLib/Board.cpp
@@ -1,1 +1,9 @@
 #include "Board.h"
+
+Board::Board()
+	:m_boardSize{24}
+{
+	for (int row = 0;row < m_boardSize;row++)
+		for (int col = 0;col < m_boardSize;col++)
+			m_board[row][col] = EPiece::None;
+}
