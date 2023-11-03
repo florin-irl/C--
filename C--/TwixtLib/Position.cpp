1f406e5133880266bd9f366e93f67802feb6d232
florin-irl <ivana.florin1987@gmail.com>
2023-11-03 15:22:19 +0200

    implement Position constructor

---
+++ C--/TwixtLib/Position.cpp
@@ -0,0 +1,8 @@
+#include "Position.h"
+
+Position::Position(int row, int col)
+	:m_row{row}
+	,m_col{col}
+{
+}
+
--- C--/TwixtLib/Position.h
+++ C--/TwixtLib/Position.h
@@ -4,6 +4,7 @@ class Position
 public:
 	Position(int row,int col);
 
+
 private:
 	int m_row;
 	int m_col;
