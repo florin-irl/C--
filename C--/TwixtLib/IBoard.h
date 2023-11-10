578f995d58eafbcd4552d7e500ffa2da1ebb5de8
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2023-11-10 11:46:14 +0200

    Update IBoard.h
    Create PlacePeg and PlaceBridge function

--- C--/TwixtLib/IBoard.h
+++ C--/TwixtLib/IBoard.h
@@ -9,6 +9,7 @@ class IBoard
 public:
 	virtual EPiece GetPiece(int line, int column) const = 0;
 	virtual std::unordered_set<Bridge> GetBridges() const = 0;
-	
+	virtual void PlacePeg(int line, int column) = 0;
+	virtual void PlaceBridge(int firstLine, int firstColumn, int secondLine, int secondColumn) = 0;
 };
 
