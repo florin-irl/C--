970b7cd888ec008e8202a145af1ea747bfdb2bd7
Mandoiu Vlad - Stefan <vlad.mandoiu@student.unitbv.ro>
2023-11-10 12:04:34 +0200

    Create GetBridges, PlacePeg and PlaceBridge functions

--- C--/TwixtLib/IGame.h
+++ C--/TwixtLib/IGame.h
@@ -9,4 +9,7 @@ class IGame
 public:
 	virtual EGameState GetGameState() = 0;
 	virtual EPiece GetPiece(int line, int column) = 0;
+	virtual std::unordered_set<Bridge> GetBridges() = 0;
+	virtual void PlacePeg(int line, int column) = 0;
+	virtual void PlaceBridge(int firstLine, int firstColumn, int secondLine, int secondColumn) = 0;
 };
