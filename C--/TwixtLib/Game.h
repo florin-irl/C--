2dea2df1ce8e0365058f2edac7292e9cdafeb0d3
Mandoiu Vlad - Stefan <vlad.mandoiu@student.unitbv.ro>
2023-11-10 12:09:33 +0200

    Create Game() constructor and add members

--- C--/TwixtLib/Game.h
+++ C--/TwixtLib/Game.h
@@ -1,9 +1,20 @@
 #pragma once
+#include "EGameState.h";
+#include "EPiece.h";
+#include "IBoard.h";
+
 class Game
 {
 public:
-
+	Game();
 private:
-
+	EGameState m_gameState;
+	EPiece m_turn;
+	int m_boardSize;
+	IBoard* m_board;
+	int m_redPegsRemaining;
+	int m_redBridgesRemaining;
+	int m_blackPegsRemaining;
+	int m_blackBridgesRemaining;
 };
 
