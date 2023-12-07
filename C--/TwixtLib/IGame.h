6b7d0ee525f5e1a5c7ef1ae9855c3da74be5aca3
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2023-12-07 21:13:53 +0200

    Create PlaceBridge and RemoveBridge

--- C--/TwixtLib/IGame.h
+++ C--/TwixtLib/IGame.h
@@ -15,6 +15,8 @@ public:
 	virtual EPiece GetPiece(int line, int column) const = 0;
 	virtual std::unordered_set<Bridge> GetBridges() const = 0;
 	virtual void PlacePeg(int line, int column) = 0; // Can throw exceptions //
+	virtual void PlaceBridge(int firstLine, int firstColumn, int secondLine, int secondColumn) = 0; // Can throw exceptions //
+	virtual void RemoveBridge(int firstLine, int firstColumn, int secondLine, int secondColumn) = 0; // Can throw exceptions //
 	virtual bool IsGameOver() const = 0;
 	virtual void SwitchTurn() = 0; // Can throw exceptions //
 	virtual ~IGame() = default;
