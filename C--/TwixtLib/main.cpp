049dd857c0c3e0bdcb67a0ce714e7dcf7af4dcde
florin-irl <ivana.florin1987@gmail.com>
2023-12-07 21:34:14 +0200

    Implement DisplayBridges

--- C--/TwixtLib/main.cpp
+++ C--/TwixtLib/main.cpp
@@ -2,6 +2,15 @@
 #include "IGame.h"
 #include "GameExceptions.h"
 
+void DisplayBridges(const std::unordered_set<Bridge>& bridges)
+{
+	for (const auto& bridge : bridges)
+	{
+		std::cout << "(" << bridge.GetFirstPegPos().GetRow() << "," << bridge.GetFirstPegPos().GetCol() << ") -> ";
+		std::cout << "(" << bridge.GetSecondPegPos().GetRow() << "," << bridge.GetSecondPegPos().GetCol() << ")\n";
+	}
+}
+
 int main()
 {
 	return 0;
