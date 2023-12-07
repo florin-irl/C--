83e7d862b9bc14d482517388eb300993a578a60c
florin-irl <ivana.florin1987@gmail.com>
2023-12-07 21:36:29 +0200

    Implement DisplayBoard

--- C--/TwixtLib/main.cpp
+++ C--/TwixtLib/main.cpp
@@ -11,6 +11,31 @@ void DisplayBridges(const std::unordered_set<Bridge>& bridges)
 	}
 }
 
+void DispayBoard(const IGamePtr& game)
+{
+	for (int i = 0; i < 24; i++)
+	{
+		std::cout << "                    ";
+		for (int j = 0; j < 24; j++)
+		{
+			if ((i == 0 && j == 0) || (i == 0 && j == 23) || (i == 23 && j == 0) || (i == 23 && j == 23))
+				std::cout << "   ";
+			else
+			{
+				if (game->GetPiece(i, j) == EPiece::RedPeg)
+					std::cout << " *";
+				else if (game->GetPiece(i, j) == EPiece::BlackPeg)
+					std::cout << " #";
+				else std::cout << " .";
+				if ((j == 0 || j == 22) && i != 0 && i != 23)
+					std::cout << "|";
+			}
+		}
+		
+	}
+	//TO UPDATE
+}
+
 int main()
 {
 	return 0;
