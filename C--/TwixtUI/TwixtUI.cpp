93ee4a96b4ecbdab1149fc311510a020b82d666c
florin-irl <ivana.florin1987@gmail.com>
2023-12-12 22:25:37 +0200

    major ui progress :alien:

--- C--/TwixtUI/TwixtUI.cpp
+++ C--/TwixtUI/TwixtUI.cpp
@@ -1,10 +1,37 @@
 #include "TwixtUI.h"
 
+
 TwixtUI::TwixtUI(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+    setWindowTitle("Twixt With a Twist! - C--");
+    m_game = IGame::CreateGame();
 }
 
 TwixtUI::~TwixtUI()
 {}
+
+void TwixtUI::paintEvent(QPaintEvent * e)
+{
+    QPainter p(this);
+    QPen pen;
+    pen.setColor(Qt::green);
+    pen.setWidth(5);
+    for (int i = 0; i < 24; i++)
+    {
+        if (i == 0 || i == 23)
+        {
+            p.drawLine(95, 80, 95, 23 * 25 + 80);
+        }
+        for (int j = 0; j < 24; j++)
+        {
+            if ((i == 0 && j == 0) || (i == 0 && j == 23) || (i == 23 && j == 0) || (i == 23 && j == 23))
+                continue;
+            QRect r(i * 25 + 75, j * 25 + 75, 15, 15);
+            p.drawEllipse(r);
+        }
+    }
+}
+
+
--- C--/TwixtUI/TwixtUI.h
+++ C--/TwixtUI/TwixtUI.h
@@ -1,8 +1,15 @@
 #pragma once
 
+#include <QMouseEvent>
+#include <QPaintEvent>
+#include <qpainter.h>
+#include <QApplication>
+#include <QRadioButton>
+#include <QVBoxLayout>
+#include <QWidget>
 #include <QtWidgets/QMainWindow>
 #include "ui_TwixtUI.h"
-
+#include "IGame.h"
 class TwixtUI : public QMainWindow
 {
     Q_OBJECT
@@ -11,6 +18,9 @@ public:
     TwixtUI(QWidget *parent = nullptr);
     ~TwixtUI();
 
+    virtual void paintEvent(QPaintEvent* e);
+
 private:
     Ui::TwixtUIClass ui;
+    IGamePtr m_game;
 };
