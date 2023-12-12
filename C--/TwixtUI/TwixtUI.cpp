632e3168e686dc8c779dda665745efddd162aa76
florin-irl <ivana.florin1987@gmail.com>
2023-12-12 22:48:51 +0200

    update ui

--- C--/TwixtUI/TwixtUI.cpp
+++ C--/TwixtUI/TwixtUI.cpp
@@ -16,19 +16,40 @@ void TwixtUI::paintEvent(QPaintEvent * e)
 {
     QPainter p(this);
     QPen pen;
-    pen.setColor(Qt::green);
-    pen.setWidth(5);
+    QBrush brush;
+    
+    pen.setWidth(2);
+
+    //draw lines
+    //left line
+    p.setPen(pen);
+    p.drawLine(93, 82, 93, 23 * 25 + 82);
+    //right line
+    p.drawLine(69 + 23 * 25, 82, 69 + 23 * 25, 23 * 25 + 82);
+    
+
     for (int i = 0; i < 24; i++)
     {
-        if (i == 0 || i == 23)
-        {
-            p.drawLine(95, 80, 95, 23 * 25 + 80);
-        }
+        
         for (int j = 0; j < 24; j++)
         {
             if ((i == 0 && j == 0) || (i == 0 && j == 23) || (i == 23 && j == 0) || (i == 23 && j == 23))
                 continue;
-            QRect r(i * 25 + 75, j * 25 + 75, 15, 15);
+            switch (m_game->GetPiece(i,j)) {
+            case EPiece::RedPeg:
+                p.setBrush(QColor(Qt::red));
+                break;
+
+            case EPiece::BlackPeg:
+                p.setBrush(QColor(Qt::black));
+                break;
+
+            case EPiece::None:
+                p.setBrush(QColor(Qt::white));
+                break;
+            }
+               
+            QRect r(i * 25 + 75, j * 25 + 75, 12, 12);
             p.drawEllipse(r);
         }
     }
