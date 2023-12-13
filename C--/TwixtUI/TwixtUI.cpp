a1427216454503dba97a4dd5dc1408581bcb0c5e
florin-irl <ivana.florin1987@gmail.com>
2023-12-14 00:20:59 +0200

    add remove bridge, implement save game

--- C--/TwixtUI/TwixtUI.cpp
+++ C--/TwixtUI/TwixtUI.cpp
@@ -174,6 +174,31 @@ void TwixtUI::mouseReleaseEvent(QMouseEvent* e)
                     }
                     else
                     {
+                        //daca deja exista bridge-ul cu nodurile selectate, il sterg
+                        try
+                        {
+                            float xselected = (m_selected.x() - 40) / 27.5;
+                            float yselected = (m_selected.y() - 40) / 27.5;
+
+                            int ceilingxselected = static_cast<int>(std::ceil(xselected));
+                            int ceilingyselected = static_cast<int>(std::ceil(yselected));
+
+                            float x = (m_coordinateMatrix[i][j].x() - 40) / 27.5;
+                            float y = (m_coordinateMatrix[i][j].y() - 40) / 27.5;
+
+                            int ceilingx = static_cast<int>(std::ceil(x));
+                            int ceilingy = static_cast<int>(std::ceil(y));
+                           /* if(m_game->)*/
+                            m_game->RemoveBridge(ceilingyselected, ceilingxselected, ceilingy, ceilingx);
+                            m_selected.setX(-1);
+                            m_selected.setY(-1);
+                        }
+                        catch (std::exception)
+                        {
+
+                        }
+
+
                         try {
                             //creez un bridge de la nodul selectat pana in punctul nou determinat;
                             float xselected = (m_selected.x() - 40) / 27.5;
@@ -209,6 +234,18 @@ void TwixtUI::mouseReleaseEvent(QMouseEvent* e)
     }
 }
 
+void TwixtUI::on_pushButton3_clicked()
+{
+    try
+    {
+        m_game->SaveGame("data.out");
+    }
+    catch (std::exception)
+    {
+        
+    }
+}
+
 void TwixtUI::initializeCoordinateMatrix()
 {
     QPainter p(this);
@@ -234,10 +271,16 @@ void TwixtUI::initializeCoordinateMatrix()
 
 void TwixtUI::on_pushButton_clicked()
 {
-    m_game->SwitchTurn();
-    m_selected.setX(-1);
-    m_selected.setY(-1);
-    update();
+    try {
+        m_game->SwitchTurn();
+        m_selected.setX(-1);
+        m_selected.setY(-1);
+        update();
+    }
+    catch (std::exception)
+    {
+
+    }
 }
 
 
--- C--/TwixtUI/TwixtUI.h
+++ C--/TwixtUI/TwixtUI.h
@@ -27,6 +27,7 @@ public:
 
 private slots:
     void on_pushButton_clicked();
+    void on_pushButton3_clicked();
 
 private:
     Ui::TwixtUIClass ui;
