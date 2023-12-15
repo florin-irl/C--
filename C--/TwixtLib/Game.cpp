53f8f0e853afff30806643064569e5c786f89d69
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2023-12-15 17:44:17 +0200

    Update PlacePeg function

--- C--/TwixtLib/Game.cpp
+++ C--/TwixtLib/Game.cpp
@@ -43,6 +43,12 @@ std::unordered_set<Bridge> Game::GetBridges() const
 
 void Game::PlacePeg(int line, int column)
 {
+	if (
+		(m_board->GetTurn() == EPiece::RedPeg && m_redPegsRemaining == 0)
+		|| (m_board->GetTurn() == EPiece::BlackPeg && m_blackPegsRemaining == 0)
+		)
+		throw NoMorePegsException("You have no pegs left !");
+
 	if (m_gameState != EGameState::Playing)
 		throw GameOverException("The game is over ! You can t place pegs anymore !");
 
