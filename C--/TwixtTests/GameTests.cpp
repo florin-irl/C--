d35cad2289ef07d30b40ca7029ec912e512f72a1
Mandoiu Vlad - Stefan <vlad.mandoiu@student.unitbv.ro>
2024-01-05 00:00:50 +0200

    update TEST(CreateGameBoard, CreateGameBoardSharePtr)

--- C--/TwixtTests/GameTests.cpp
+++ C--/TwixtTests/GameTests.cpp
@@ -216,6 +216,20 @@ TEST(CreateGameBoard, CreateGameBoardSharePtr)
 
 	EXPECT_EQ(g->GetTurn(), EPiece::RedPeg);
 
-	
+	for (size_t i = 0; i < 24; i++)
+	{
+		for (size_t j = 0; j < 24; j++)
+		{
+			EXPECT_EQ(g->GetPiece(i, j), EPiece::None);
+		}
+	}
+	EXPECT_EQ(g->GetBoardSize(), 24);
+
+	EXPECT_EQ(g->GetNrBlackPegsRemaining(), 50);
+	EXPECT_EQ(g->GetNrRedPegsRemaining(), 50);
+	EXPECT_EQ(g->GetNrBlackBridgesRemaining(), 50);
+	EXPECT_EQ(g->GetNrRedBridgesRemaining(), 50);
+
+	EXPECT_EQ(g->GetGameState(), EGameState::Playing);
 }
 
